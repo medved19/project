7c995f53a31c14a19ff43d209c70f86a3ed51303
misha <mishakozko@gmail.com>
2023-03-05 14:20:25 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const answers = [];
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('How old are you?', '');
+answers[2] = prompt('What is your gender?', '');
+
+document.write(answers);
